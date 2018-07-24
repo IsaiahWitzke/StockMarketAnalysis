9c45a8087aaba37eb0a942a90dc43382ab1e9071
Isaiah <witzkei@yahoo.com>
2018-07-24 13:28:21 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace StockMarketAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Historical Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(97, 101);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
+            series1.YValuesPerPoint = 4;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(585, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+
+namespace StockMarketAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnMouseWheel(MouseEventArgs mouseEvent)
+        {
+            //zooming in
+            if (mouseEvent.Delta > 0)
+            {
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoom(1, 1);
+            }
+
+            //zooming out
+            if (mouseEvent.Delta < 0)
+            {
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //getting the data from online:
+            string symbol = textBox1.Text;
+            string rawDataPath = "C:/Users/witzk/source/repos/StockMarketAnalysis/StockMarketAnalysis/RawData/";
+            string strCmdText;
+            strCmdText = "/C alpha-vantage-cli -s " + symbol + " -k TPMQDECWM5ATUR1L -o " + rawDataPath + symbol;
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+            //reading the output file:
+            using (var reader = new StreamReader(rawDataPath + symbol))
+            {
+                bool isFirstLine = true;
+                while (!reader.EndOfStream)
+                {
+                    if (isFirstLine)
+                    {
+                        reader.ReadLine();
+                        isFirstLine = false;
+                    }
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    //date stuff
+                    var stringDateArr = values[0].Split('-');
+                    int[] dateInfo = new int[3];
+                    dateInfo[0] = Convert.ToInt32(stringDateArr[0]);
+                    dateInfo[1] = Convert.ToInt32(stringDateArr[1]);
+                    dateInfo[2] = Convert.ToInt32(stringDateArr[2]);
+
+                    chart1.Series[0].XValueType = ChartValueType.DateTime;
+                    System.DateTime x = new System.DateTime(dateInfo[0], dateInfo[1], dateInfo[2]);
+                    //chart1.Series[0].Points.AddXY(x.ToOADate(), 34);
+
+                    //candle stick data
+                    double open = Convert.ToDouble(values[1]);
+                    double high = Convert.ToDouble(values[2]);
+                    double low = Convert.ToDouble(values[3]);
+                    double close = Convert.ToDouble(values[4]);
+                    double[] data = { open, high, low, close };
+                    DataPoint candleStick = new DataPoint(x.ToOADate(), data);
+                    chart1.Series["Series1"].Points.Add(candleStick);
+                }
+            }            
+        }
+
+    }
+}
